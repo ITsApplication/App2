5bf4cd9a85176daa406e331004cd43704652c13e
Saša Dejanovic <xodium@hotmail.com>
2018-02-22 21:11:15 +0100

    First Try

---
+++ App2/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get 
+            EditText emailBox = FindViewById<EditText>(Resource.Layout.Main);
+            EditText passwordBox = FindViewById<EditText>(Resource.Layout.Main);
+
+            // MAKE EMAILBOX STRING INTO BOOLEAN, THEN CHECK IF TRUE OR FALSE
+
+            bool emailBool;
+
+
+            if (emailBool == 0)
+            {
+                System.Console.WriteLine("Enter a valid email adress!");
+            }
+            
+        }
+    }
+}
+
